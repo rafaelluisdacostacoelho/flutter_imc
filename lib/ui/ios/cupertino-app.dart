d330ef9f87b3aa9060082fdf58ddd19adab24155
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2019-11-05 17:34:29 -0300

    Adiciona a base do projeto funcional.

---
+++ lib/ui/ios/cupertino-app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyCupertinoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      
+    );
+  }
+}
